b9ceb6e9f03e328d892076487b59a21bd7bee08e
Ramiro Krapp <krappramiro@disroot.org>
2022-08-26 16:17:28 -0300

    deleted the drop_all from init_db

--- init_db.py
+++ init_db.py
@@ -4,7 +4,6 @@ from src import db
 from src.routes import Movie
 
 # HACK, ese drop_all es bastante feo, es para "reiniciar" la base de datos
-db.drop_all()
 db.create_all()
 
 if Movie.query.count() == 0:
